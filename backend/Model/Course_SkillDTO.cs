a1a13b11fe1cab75f357aa7854ff0b6411d8bb1f
Hung <hugo08092000@gmail.com>
2023-01-01 10:45:49 +0700

    API: Xem lịch sử (kĩ năng)
    Thêm Course_SkillDTO

---
+++ backend/Model/Course_SkillDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Model
+{
+    public class Course_SkillDTO
+    {
+        public string idSkill { get; set; }
+        public string idCourse { get; set; }
+    }
+}
